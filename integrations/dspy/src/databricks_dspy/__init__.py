530a4e75a8f5d5e8e4ea933b24324e93fe0044d1
Bradley Jamrozik <bradleyjamrozik@ozinga.com>
2025-08-04 11:06:14 -0500

    clean up old files

--- integrations/dspy/src/databricks_dspy/__init__.py
+++ integrations/dspy/src/databricks_dspy/__init__.py
@@ -1,3 +1,3 @@
-from databricks_dspy.clients import DatabricksLM, GenieTool, GenieToolResponse, GenieResponse
+from databricks_dspy.clients import DatabricksLM
 
-__all__ = ["DatabricksLM", "GenieTool", "GenieToolResponse", "GenieResponse"]
+__all__ = ["DatabricksLM"]
--- integrations/dspy/src/databricks_dspy/clients/__init__.py
+++ integrations/dspy/src/databricks_dspy/clients/__init__.py
@@ -1,4 +1,3 @@
 from databricks_dspy.clients.databricks_lm import DatabricksLM
-from databricks_dspy.clients.genie import GenieTool, GenieToolResponse, GenieResponse
 
-__all__ = ["DatabricksLM", "GenieTool", "GenieToolResponse", "GenieResponse"]
+__all__ = ["DatabricksLM"]
